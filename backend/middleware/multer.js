d883e696e8b86df26d538e7eac8d9f9263eaa464
ChetanKoliparthi <chetankoliparthi@gmail.com>
2025-02-19 22:18:34 +0530

    multer changed

--- backend/middleware/multer.js
+++ backend/middleware/multer.js
@@ -1,24 +1,9 @@
 import multer from "multer";
-import path from "path";
-import fs from "fs";
 
-// Ensure the 'uploads' folder exists
-const uploadDir = "uploads/";
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-}
-
-// Configure storage
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, uploadDir); // Files will be stored in 'uploads' directory
-    },
-    filename: function (req, file, cb) {
-        cb(null, Date.now() + path.extname(file.originalname)); // Unique filename
+    filename: function (req,file,callback){
+        callback(null,file.originalname);
     }
-});
-
-// Initialize Multer
-const upload = multer({ storage });
-
+})
+const upload = multer({storage})
 export default upload;
